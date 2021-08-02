6eae498a0d7e7c3a343c5ebcc0e569297b7f9ae2
Marcos Capistrano <mcapistrano50@gmail.com>
2021-08-02 17:47:03 -0300

    feat(teste): teste

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('oi');
+});
+
 console.log('oie');
 console.log('oie');
+
+const oi = 10;
+console.log(oi);
